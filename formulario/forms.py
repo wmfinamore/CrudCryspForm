88a6a0f6c2232cdf9889f344097c5e0bd72da871
Willian Moreira Finamore <wmfinamore@hotmail.com>
2023-02-04 22:58:43 -0300

    Refactored AddressForm form class to use crispy FormHelper

--- formulario/forms.py
+++ formulario/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 
 STATES = (
@@ -18,3 +20,24 @@ class AddressForm(forms.Form):
     state = forms.ChoiceField(choices=STATES)
     zip_code = forms.CharField(label='Zip')
     check_me_out = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('email', css_class='form-group col-md-6 mb-0'),
+                Column('password', css_class='form-group col-md-6 md-0'),
+                css_class='form-row'
+            ),
+            'address_1',
+            'address_2',
+            Row(
+                Column('city', css_class='form-group col-md-6 mb-0'),
+                Column('state', css_class='form-group col-md-4 mb-0'),
+                Column('zip_code', css_class='form-group col-md-2 mb-0'),
+                css_class='form-row'
+            ),
+            'check_me_out',
+            Submit('submit', 'Sign in')
+        )
